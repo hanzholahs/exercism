535519220ba4ababbba0d249aa2f7f0564dc2222
hanzholahs <hanzholahs@gmail.com>
2024-01-06 19:03:16 +0700

    learn julia darts

---
+++ julia/darts/darts.jl
@@ -0,0 +1,14 @@
+
+function score(x, y)
+    r = sqrt(x^2 + y^2)
+
+    if r ≤ 1
+        return 10
+    elseif r ≤ 5
+        return 5
+    elseif r ≤ 10
+        return 1
+    end
+    
+    return 0
+end
