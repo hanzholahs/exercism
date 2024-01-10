333f1414eee784d2fd39a73685757d8b0b7fbf06
hanzholahs <hanzholahs@gmail.com>
2024-01-10 14:12:33 +0700

    learn julia raindrops iteration 2

--- julia/raindrops/raindrops.jl
+++ julia/raindrops/raindrops.jl
@@ -1,12 +1,9 @@
 function raindrops(number)
-    if (number % 3 > 0) & (number % 5 > 0) & (number % 7 > 0) return string(number) end
-
     out = ""
 
-    if (number % 3 == 0) out = out * "Pling" end
-    if (number % 5 == 0) out = out * "Plang" end
-    if (number % 7 == 0) out = out * "Plong" end
+    if (number % 3 == 0) out *= "Pling" end
+    if (number % 5 == 0) out *= "Plang" end
+    if (number % 7 == 0) out *= "Plong" end
 
-    return out 
-
+    return out == "" ? string(number) : out
 end
